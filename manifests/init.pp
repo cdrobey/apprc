bcaa1d23fb1f581cb56e5e7217a75cf75ee7e35a
Chris Roberson <cdroberson@gmail.com>
2017-09-20 13:02:12 -0600

    Initial Commit.

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+# apprc
+#
+# Install and configure rc script for the defined application with startup validation.
+#
+# @summary  This class creates a mechanism to manage rc scripts for applications on RHEL/Centos7
+#           system.  The script includes the capability to add a post-exec test validationn script
+#           to ensure the health of the application after startup.
+#
+# @example
+#   include apprc
+class apprc {
+}
