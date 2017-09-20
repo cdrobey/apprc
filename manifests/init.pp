0f008624903c794263ed13d35da6af4aa51035d3
Chris Roberson <cdroberson@gmail.com>
2017-09-20 14:12:13 -0600

    Created class to install and enable application service.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,25 @@
 #           system.  The script includes the capability to add a post-exec test validationn script
 #           to ensure the health of the application after startup.
 #
+# @param rc_startid [String] Allows for the customization of priority given to the script during startup.
+# @param rc_stopid [String] Allows for the customization of priority given to the script during shutdown.
+# @param rc_template [String] Allows for custom template location for rc script
+# @param rc_valiation [String] Allows for custom template location for validation script
+#
 # @example
 #   include apprc
-class apprc {
+class apprc (
+  $service_name = test,
+){
+    file { '/etc/init.d/$service_name':
+        ensure  => file,
+        #content => epp('service.epp', {'apprc/$service_name' => '$service_name'}),
+        content => epp('apprc/apprc_service.epp'),
+    }
+    service { '$service_name':
+        ensure     => 'running',
+        hasrestart => true,
+        hasstatus  => true,
+        enable     => true,
+    }
 }
